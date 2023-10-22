b4e1b51f654161219b6a72c06b76c92ba746ae15
rgandhi9 <roshangandhi@hotmail.co.uk>
2023-10-22 22:39:27 +0100

    review terraform plan

--- main.tf
+++ main.tf
@@ -1,122 +1,45 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 4.34.0"
-    }
-  }
+# Define the required providers
+provider "google" {
+  project = "tough-flow-388709"
+  region  = "us-east1"
 }
 
-resource "random_id" "bucket_prefix" {
-  byte_length = 4
-}
+# Create a GCS bucket
+resource "google_storage_bucket" "iss_data_bucket" {
+  name     = "iss_data"
+  location = "us-east1"
 
-resource "google_storage_bucket" "source_bucket" {
-  name                        = "${random_id.bucket_prefix.hex}-gcf-source-bucket"
-  location                    = "US"
+  # Set the roles and permissions for the bucket
   uniform_bucket_level_access = true
 }
 
-data "archive_file" "default" {
-  type        = "zip"
-  output_path = "/tmp/function-source.zip"
-  source_dir  = "terraform_cf/"
+# Create a Pub/Sub topic
+resource "google_pubsub_topic" "gcs_trigger_topic" {
+  name = "gcs-trigger-topic"
 }
 
-resource "google_storage_bucket_object" "default" {
-  name   = "function-source.zip"
-  bucket = google_storage_bucket.source_bucket.name
-  source = data.archive_file.default.output_path # Path to the zipped function source code
+# Grant roles to the service account for GCS bucket
+resource "google_project_iam_binding" "gcs_iam_binding" {
+  project = "tough-flow-388709"
+  role    = "roles/storage.admin"
+  members = ["serviceAccount:test_sa@tough-flow-388709.iam.gserviceaccount.com"]
 }
 
-resource "google_storage_bucket" "trigger_bucket" {
-  name                        = "${random_id.bucket_prefix.hex}-gcf-trigger-bucket"
-  location                    = "us-east1" # The trigger must be in the same location as the bucket
-  uniform_bucket_level_access = true
+# Grant roles to the service account for Pub/Sub topic
+resource "google_project_iam_binding" "pubsub_iam_binding" {
+  project = "tough-flow-388709"
+  role    = "roles/pubsub.admin"
+  members = ["serviceAccount:test_sa@tough-flow-388709.iam.gserviceaccount.com"]
 }
 
-data "google_storage_project_service_account" "default" {
+# Create a notification for the GCS bucket
+resource "google_storage_notification" "gcs_trigger" {
+  bucket = google_storage_bucket.iss_data_bucket.name
+
+  topic = google_pubsub_topic.gcs_trigger_topic.name
+
+  payload_format = "JSON_API_V1"
+
+  event_types = ["OBJECT_FINALIZE"]
 }
 
-data "google_project" "project" {
-}
-
-resource "google_project_iam_member" "gcs_pubsub_publishing" {
-  project = data.google_project.project.project_id
-  role    = "roles/pubsub.publisher"
-  member  = "serviceAccount:${data.google_storage_project_service_account.default.email_address}"
-}
-
-resource "google_service_account" "account" {
-  account_id   = "gcf-sa"
-  display_name = "Test Service Account - used for both the cloud function and eventarc trigger in the test"
-}
-
-# Permissions on the service account used by the function and Eventarc trigger
-resource "google_project_iam_member" "invoking" {
-  project    = data.google_project.project.project_id
-  role       = "roles/run.invoker"
-  member     = "serviceAccount:${google_service_account.account.email}"
-  depends_on = [google_project_iam_member.gcs_pubsub_publishing]
-}
-
-resource "google_project_iam_member" "event_receiving" {
-  project    = data.google_project.project.project_id
-  role       = "roles/eventarc.eventReceiver"
-  member     = "serviceAccount:${google_service_account.account.email}"
-  depends_on = [google_project_iam_member.invoking]
-}
-
-resource "google_project_iam_member" "artifactregistry_reader" {
-  project    = data.google_project.project.project_id
-  role       = "roles/artifactregistry.reader"
-  member     = "serviceAccount:${google_service_account.account.email}"
-  depends_on = [google_project_iam_member.event_receiving]
-}
-
-resource "google_cloudfunctions2_function" "default" {
-  depends_on = [
-    google_project_iam_member.event_receiving,
-    google_project_iam_member.artifactregistry_reader,
-  ]
-  name        = "function"
-  location    = "us-east1"
-  description = "move file from one gcs bucket to another on event/trigger"
-
-  build_config {
-    runtime     = "python39"
-    entry_point = "function_entry" # Set the entry point in the code
-    environment_variables = {
-      BUILD_CONFIG_TEST = "build_test"
-    }
-    source {
-      storage_source {
-        bucket = google_storage_bucket.source_bucket.name
-        object = google_storage_bucket_object.default.name
-      }
-    }
-  }
-
-  service_config {
-    max_instance_count = 3
-    available_memory   = "128M"
-    timeout_seconds    = 30
-    environment_variables = {
-      SERVICE_CONFIG_TEST = "config_test"
-    }
-    ingress_settings               = "ALLOW_INTERNAL_ONLY"
-    all_traffic_on_latest_revision = true
-    service_account_email          = google_service_account.account.email
-  }
-
-  event_trigger {
-    trigger_region        = "us-east1" # The trigger must be in the same location as the bucket
-    event_type            = "google.cloud.storage.object.v1.finalized"
-    retry_policy          = "RETRY_POLICY_RETRY"
-    service_account_email = google_service_account.account.email
-    event_filters {
-      attribute = "bucket"
-      value     = google_storage_bucket.trigger_bucket.name
-    }
-  }
-}
